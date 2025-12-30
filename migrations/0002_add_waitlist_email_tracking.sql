6641d5d0308e85477639dad3d6b0f9e302f26f65
Neme88 <ccnwaka1988@gmail.com>
2025-12-30 16:49:40 +0200

    fix: added email tracking columns inside the waitlist table

--- migrations/0002_add_waitlist_email_tracking.sql
+++ migrations/0002_add_waitlist_email_tracking.sql
@@ -5,3 +5,7 @@ ALTER TABLE waitlist ADD COLUMN email_status TEXT;
 ALTER TABLE waitlist ADD COLUMN email_error TEXT;
 
 ALTER TABLE waitlist ADD COLUMN email_sent_at TEXT;
+
+ALTER TABLE waitlist ADD COLUMN email_attempts INTEGER DEFAULT 0;
+
+ALTER TABLE waitlist ADD COLUMN next_email_attempt_at TEXT;
